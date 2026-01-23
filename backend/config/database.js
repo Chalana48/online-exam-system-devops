dba4247aba2db33ef40d51433348062aa099dbe6
BinukaJayakody <binukajayakody@gmail.com>
2026-01-23 18:44:09 +0530

    Delete backend directory

--- backend/config/database.js
+++
@@ -1,7 +0,0 @@
-const connectDB = async () => {
-    console.log('✅ Using in-memory database for testing');
-    console.log('⚠️ Note: Data will be lost when server restarts');
-    return Promise.resolve();
-};
-
-module.exports = connectDB;
